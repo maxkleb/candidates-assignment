98822b87810d4e18ef1d0f73f44a80129ef94828
Max <maxkleb@gmail.com>
2019-08-24 13:56:19 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise1 here
+echo "------------------EX1-----------------"
+sudo ip route del 208.86.224.90
+exit_code=$?
+if [ $exit_code -eq "0" ];
+	then 
+	curl http://www.textfiles.com/art/bnbascii.txt
+else
+	echo "Cannot delete the route, something went wrong!!!"
